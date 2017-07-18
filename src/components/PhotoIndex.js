dfe2ed09d063fb172d7a2c5ca31706c1bfc04a45
Tim Chase <tchase4544@gmail.com>
2017-07-18 10:00:34 -0400

    react up, adding routes and components

---
+++ src/components/PhotoIndex.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class PhotoIndex extends Component{
+
+	render() {
+		return (
+			<div>
+				<h1>Im the photo competition index page</h1>
+			</div>
+		);
+	}
+}
+
+export default PhotoIndex
---
+++ src/components/VideoIndex.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class VideoIndex extends Component{
+
+	render() {
+		return (
+			<div>
+				<h1>Im the video index page</h1>
+			</div>
+		);
+	}
+}
+
+export default VideoIndex
